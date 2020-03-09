2e5cbf1bb1b96cfeb80f468d763739bf82a048a7
Roberto Scudeller <beto.rvs@gmail.com>
2020-03-09 14:15:01 +0100

    correct bash deploy script

--- travis_deploy.sh
+++ travis_deploy.sh
@@ -9,7 +9,7 @@ docker build -f Dockerfile -t ${IMAGE}:${TAG} .
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker push ${IMAGE}:$TAG
-if ["${LATEST}" = "true" ]; then 
+if [ "${LATEST}" = "true" ]; then 
   docker tag ${IMAGE}:$TAG ${IMAGE}:latest
   docker push ${IMAGE}:latest
 fi
